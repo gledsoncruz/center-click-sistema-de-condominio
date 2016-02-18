88ef280ec17cf4a1d06f2103586a1cf30a2439f5
Gledson Cruz <gledson.cruz@gmail.com>
2016-02-18 21:47:02 -0200

    testando variavel ambiente

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
       return version
     end
   end
+
+  def show_version_date
+    version_date = ENV['HEROKU_RELEASE_CREATED_AT']
+    if version_date.to_s.strip.length == 0
+      # It's nil, empty, or just whitespace
+      return 'NÃO ESPECIFICADA'
+    else
+      return version_date
+    end
+  end
 end
